bf91ff0995fd288d2d087c470772328b59c5ffea
boomchanotai <boom9387@hotmail.com>
2025-04-03 22:57:09 +0700

    feat: tooltip

--- src/components/generative/components/prompt-column-card.tsx
+++ src/components/generative/components/prompt-column-card.tsx
@@ -1,10 +1,15 @@
-import { Dispatch, SetStateAction } from "react"
-
 import { PromptColumn } from "@/types"
+import { Info } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 interface PromptColumnCardProps {
   col: PromptColumn
@@ -29,16 +34,29 @@ export const PromptColumnCard = (props: PromptColumnCardProps) => {
       className="space-y-4 rounded-md border bg-white/50 p-4"
       onSubmit={handleSubmit}
     >
-      <Label>Prompt for {col.name}</Label>
+      <div className="flex items-center gap-2">
+        <Label>Prompt for {col.name}</Label>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Info className="size-4" />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{"Use {{column_name}} to refer text from your column"}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <Input
         value={col.prompt}
         onChange={(e) => {
           const value = e.target.value
           handleSetColumnPrompt(col.id, value)
         }}
-        placeholder="prompt"
+        placeholder="generate {{column_name}} description for me"
         required
       />
+      <p className="text-xs text-gray-500"></p>
       <div className="flex justify-end">
         <Button type="submit" className="gap-2" disabled={isGenerating}>
           Generate Column
