e0688441b9754558bc00e54adb04b2992750c133
boomchanotai <boom9387@hotmail.com>
2025-04-06 12:42:07 +0700

    feat: demo

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,3 +1,4 @@
+import { Container } from "../common/container"
 import { Demo } from "./components/demo"
 import { Hero } from "./components/hero"
 import { Privacy } from "./components/privacy"
@@ -7,6 +8,18 @@ export const Home = () => {
   return (
     <div className="space-y-16 py-8">
       <Hero />
+      <Container>
+        <div className="mx-auto w-full md:w-3/4">
+          <iframe
+            className="aspect-video w-full"
+            src="https://www.youtube.com/embed/RjQiMhi-yGk?si=kNknjVVpHVjivmQv"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </Container>
       <Steps />
       <Privacy />
       <Demo />
